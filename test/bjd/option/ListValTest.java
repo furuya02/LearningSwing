5e18119f06323a430bc6670e06cfc709d354431c
furuya02 <sin@sapporoworks.ne.jp>
2012-09-07 12:49:30 +0900

    2012.09.07.1250

--- test/bjd/option/ListValTest.java
+++ test/bjd/option/ListValTest.java
@@ -1,10 +1,42 @@
 package bjd.option;
 
+import java.util.ArrayList;
+
+import junit.framework.Assert;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-@RunWith(Enclosed.class)
+import bjd.ctrl.CtrlInt;
+import bjd.util.TestUtil;
+
+//@RunWith(Enclosed.class)
 public class ListValTest {
+    
+    @Test
+    public void A001() {
+        TestUtil.dispHeader("A001 複数のOneValをadd()で追加した後、getList（)で取得した値が正しいかをテストする");
+
+        ListVal listVal = new ListVal();
+        listVal.add(new OneVal("n1",1,Crlf.NEXTLINE, new CtrlInt("help",10)));
+        listVal.add(new OneVal("n2",1,Crlf.NEXTLINE, new CtrlInt("help",10)));
+        
+        ArrayList<OneVal> list = listVal.getList(null);
+        StringBuilder sb = new StringBuilder();
+        for(OneVal o : list){
+            sb.append(o.getName());
+            sb.append(",");
+        }
+        String expected = "n1,n2,";
+
+        System.out.printf("listVal.getList(null)=%s expected=%s\n",sb.toString(),expected);
+        assertThat(sb.toString(),is(expected));
+    }
+
+    
 
 //	@RunWith(Theories.class)
 //	public static class A001 {
